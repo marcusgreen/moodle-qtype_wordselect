fcd186fbd27183040f79bcacb9fd2d6863568f81
unknown <marcusavgreen@gmail.com>
2016-05-30 13:03:45 +0100

    Lots of changes, now gets param for grade_response

--- question.php
+++ question.php
@@ -25,7 +25,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
+/* require_once($CFG->dirroot . '/question/type/questionbase.php'); */
+
 require_once('Kint/Kint.class.php');
+
 /**
  * Represents a wordselect question.
  *
@@ -34,26 +37,52 @@ require_once('Kint/Kint.class.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qtype_wordselect_question extends question_graded_automatically_with_countback {
-    
-    
-    public function start_attempt(question_attempt_step $step, $variant) {
-        Kint::dump($step);
-        exit();
-        $this->order = array_keys($this->answers);
-        $step->set_qt_var('_order', implode(',', $this->order));
+
+
+    public function compare_response_with_answer(array $response, question_answer $answer) {
+        
     }
 
+     /**
+     * @param int $key stem number
+     * @return string the question-type variable name.
+     */
+    public function field($place) {
+        return 'p' . $place;
+    }
+
+    public function is_gradable_response(array $response) {
+        return true;
+    }
+
+    public function get_all_words() {
+        $questiontextnodelim = $this->questiontext;
+        $questiontextnodelim = preg_replace('/\[/', '', $questiontextnodelim);
+        $questiontextnodelim = preg_replace('/\]/', '', $questiontextnodelim);
+        $allwords = preg_split('/[\s\n]/', $questiontextnodelim);
+        return $allwords;
+    }
+
+    /**
+     * Return an array of the question type variables that could be submitted
+     * as part of a question of this type, with their types, so they can be
+     * properly cleaned.
+     * @return array variable name => PARAM_... constant.
+     */
     public function get_expected_data() {
-        // TODO.
-        return array();
+        $wordcount = sizeof($this->get_all_words());
+        for ($key = 0; $key < $wordcount; $key++) {
+            $data['p' . $key] = PARAM_RAW_TRIMMED;
+        }
+        return $data;
     }
 
     public function summarise_response(array $response) {
-        // TODO.
-        return null;
+        
     }
 
     public function is_complete_response(array $response) {
+
         // TODO.
         return true;
     }
@@ -64,9 +93,18 @@ class qtype_wordselect_question extends question_graded_automatically_with_count
     }
 
     public function is_same_response(array $prevresponse, array $newresponse) {
-        // TODO.
-        return question_utils::arrays_same_at_key_missing_is_blank(
-                        $prevresponse, $newresponse, 'answer');
+        /* if you are moving from viewing one question to another this will
+         * discard the processing if the answer has not changed. If you don't
+         * use this method it will constantantly generate new question steps and
+         * the question will be repeatedly set to incomplete. This is a comparison of
+         * the equality of two arrays. Without this deferred feedback behaviour probably
+         * wont work.
+         */
+        if ($prevresponse == $newresponse) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function get_correct_response() {
@@ -75,35 +113,43 @@ class qtype_wordselect_question extends question_graded_automatically_with_count
     }
 
     public function check_file_access($qa, $options, $component, $filearea, $args, $forcedownload) {
-        // TODO.
-        if ($component == 'question' && $filearea == 'hint') {
-            return $this->check_hint_file_access($qa, $options, $args);
-        } else {
-            return parent::check_file_access($qa, $options, $component, $filearea, $args, $forcedownload);
-        }
+        
     }
 
+    /**
+     * @param array $response responses, as returned by
+     *      {@link question_attempt_step::get_qt_data()}.
+     * @return array (number, integer) the fraction, and the state.
+     */
     public function grade_response(array $response) {
-        Kint::dump($response);
-        exit();
-         // TODO.
-        $fraction = 0;
-        return array($fraction, question_state::graded_state_for_fraction($fraction));
+        $right = 0;
+        print "grade response";
+        $allwords = $this->get_all_words();
+
+        foreach ($response as $index => $value) {
+            $responsewords[] = $allwords[substr($index, 1, 99)];
+        }
+
+        foreach ($responsewords as $response) {
+            foreach ($this->answers as $answer) {
+                if($answer->answer===$response){
+                    $right++;
+                    break;
+                }else{
+                    $right--;
+                    break;
+                }                
+            }
+        }
+  
+        $fraction = $right / sizeof($this->answers);
+        $grade = array($fraction, question_state::graded_state_for_fraction($fraction));
+        return $grade;
     }
 
     public function compute_final_grade($responses, $totaltries) {
         // TODO.
         return 0;
-    }
-
-    public function get_text_chunks(question_attempt $qa) {
-        $question = $qa->get_question();
-        $questiontext = $question->questiontext;
-        $questiontext = preg_replace("/\[/", "", $questiontext);
-        $questiontext = preg_replace("/\]/", "", $questiontext);
-
-        $words = preg_split("/[\s,]+/", $questiontext);
-        return $words;
     }
 
 }
