d7db6be7df46abd2791774e1fb341224ccec0608
unknown <marcusavgreen@gmail.com>
2016-06-06 21:51:29 +0100

    commented out kint reference

--- questiontype.php
+++ questiontype.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->libdir . '/questionlib.php');
 require_once($CFG->dirroot . '/question/engine/lib.php');
 require_once($CFG->dirroot . '/question/type/wordselect/question.php');
-require_once('Kint/Kint.class.php');
+//require_once('Kint/Kint.class.php');
 
 /**
  * The wordselect question type.
