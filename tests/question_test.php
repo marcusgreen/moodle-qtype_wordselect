06aae644f0eae3a7c3d9a395c20e7f5d85048e60
Marcus Green <marcusavgreen@gmail.com>
2016-10-29 22:41:02 +0100

    7 test, 10 assertions

--- tests/question_test.php
+++ tests/question_test.php
@@ -52,5 +52,55 @@ class qtype_wordselect_question_test extends UnitTestCase {
          $response = array('p2'=>'on');
          $this->assertEquals(($question->summarise_response($response)), ' sat ');
     }
- 
+  public function test_grade_response() {
+        $questiontext='The cat [sat]';
+        $question = qtype_wordselect_test_helper::make_question('wordselect',$questiontext);  
+        $response = array('p2' => 'on');
+        list($fraction, $state) = $question->grade_response($response);
+        $this->assertEquals($fraction, 1);
+        /* This question type uses subtractive marking to mitigate any benefit from a 
+         * strategy of click on everything to get full marks. The mark for any incorrect selections
+         * is deducted from the marks for correct selections down to zero.
+         */
+        $questiontext='The cat [sat] and the cow [jumped]';
+        $question = qtype_wordselect_test_helper::make_question('wordselect',$questiontext);  
+        $response = array('p2' => 'on','p6'=>'off');
+        list($fraction, $state) = $question->grade_response($response);
+        $this->assertEquals($fraction, .5);
+        
+    }
+    
+    public function test_is_complete_response() {
+        $questiontext='The cat [sat] and the cow [jumped]';
+        $question = qtype_wordselect_test_helper::make_question('wordselect',$questiontext);
+        /*
+         * A response is considered complete if there is at least one item selected. In this
+         * case it is a "correct" item, i.e. one with delimitcharacters but it doesn't have to be
+         * 
+         */      
+        $response = array('p2' => 'on');
+        $this->assertTrue($question->is_complete_response($response));
+        /* this time nothing is selected */
+        $response = array();
+        $this->assertFalse($question->is_complete_response($response));
+    }
+    public function test_is_correct_place() {
+        $question = qtype_wordselect_test_helper::make_question('wordselect');
+        $correctplaces = $question->get_correct_places($question->questiontext, "[]");
+        $this->assertTrue($question->is_correct_place($correctplaces,2));        
+    }
+    public function test_is_word_selected() {
+        $question = qtype_wordselect_test_helper::make_question('wordselect');
+        $response = array('p1' => 'on');
+        $this->assertTrue($question->is_word_selected(1,$response)); 
+        $response = array('1' => 'on');
+        $this->assertFalse($question->is_word_selected(1,$response)); 
+    }
+      public function test_get_correct_places() {
+        $question = qtype_wordselect_test_helper::make_question('wordselect');  
+        /* counting from 0 the correct place is 2 (i.e. the word sat) */
+        $correctplaces = ['0' => 2];
+        $this->assertEquals($question->get_correct_places($question->questiontext,'[]'),$correctplaces);
+    }
+
 }
