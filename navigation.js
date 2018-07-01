233fb14507b63bb2d88c677519a1f8c798410c6a
marcusgreen <marcusavgreen@gmail.com>
2018-07-01 20:57:27 +0100

    hide questiontext and add next previous buttons

--- navigation.js
+++ navigation.js
@@ -1,15 +1,16 @@
 
 $('.collapsible-actions').hide();
 $navbuttons =
-       '<button id="nav_combined">General feedback</button>\
+        '<button id="nav_previous">Previous</button>\
+        <button id="nav_combined">General feedback</button>\
         <button id="nav_combined">Combined feedback</button>\
         <button id="nav_multitries">Multi tries</button>\
-        <button id="nav_tags">Tags</button>';
-
+        <button id="nav_tags">Tags</button>\
+        <button id="nav_next">Next</button>';
 
 
 $($navbuttons).insertAfter('.collapsible-actions');
-
+$("#id_questiontext").closest('.form-group').hide();
 $('#id_generalfeedback').closest('.form-group').hide();
 $('#id_delimitchars').closest('.form-group').hide();
 
