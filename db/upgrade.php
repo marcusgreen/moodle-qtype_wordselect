b3f98782a7e13ad001d3244d1aeb1a92877f5d08
marcusgreen <marcusavgreen@gmail.com>
2018-07-01 14:07:27 +0100

    codechecker conformance (line length)

--- db/upgrade.php
+++ db/upgrade.php
@@ -32,7 +32,8 @@ function xmldb_qtype_wordselect_upgrade($oldversion = 0) {
     $dbman = $DB->get_manager();
     if ($oldversion < 2018070100) {
         if (!$dbman->field_exists('question_wordselect', 'wordpenalty')) {
-            $field = new xmldb_field('wordpenalty', XMLDB_TYPE_NUMBER, '12, 8', null, XMLDB_NOTNULL, null, '0.33333', 'delimitchars');
+            $field = new xmldb_field('wordpenalty', XMLDB_TYPE_NUMBER, '12, 8',
+                    null, XMLDB_NOTNULL, null, '0.33333', 'delimitchars');
             $table = new xmldb_table('question_wordselect');
             $dbman->add_field($table, $field);
         }
