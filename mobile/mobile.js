a93f513bec757bec53755115ad50ee773a4f1490
marcusgreen <marcusavgreen@gmail.com>
2018-12-20 20:25:50 +0000

    support for mobile app (3.5 version)

---
+++ mobile/mobile.js
@@ -0,0 +1,109 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * support for the mdl35+ mobile app
+ * This file is the equivalent of
+ * qtype/wordselect/classes/wordselect.ts in the core app
+ * e.g.
+ * https://github.com/moodlehq/moodlemobile2/blob/v3.5.0/src/addon/qtype/ddwtos/classes/ddwtos.ts
+ */
+
+
+var that = this;
+var result = {
+
+    componentInit: function() {
+
+        function selectWord(event) {
+            var selector = "#" + event.target.id;
+            var parts = selector.split(":");
+            selector = parts[0] + "\\:" + parts[1];
+            var selection = document.querySelector(selector + ".selectable");
+            if (selection == null){
+                /* selection will be null after marking/readonly */
+                return;
+            }
+            /* not sure if this is necessary */
+            var hidden = document.getElementById(selection.getAttribute('id'))
+            if (selection.classList.contains('selected')) {
+                selection.classList.remove('selected');
+                selection.title = '';
+                selection.setAttribute("aria-checked",false)
+                /* Change the type to text to avoid the problem that
+                unchecked checkboxes are not passed in the request */
+                hidden.style.visibility = "hidden";
+                hidden.style.display = "none";
+                hidden.value = "";
+                hidden.checked = "false";
+
+            } else {
+                selection.classList.add('selected');
+                selection.title = 'selected';
+                selection.setAttribute("aria-checked",true)
+                hidden.value = "on";
+                selection.value = "on";
+                hidden.checked = "true";
+            }
+        }
+
+        this.questionRendered = function questionRendered() {
+            var selchecks = this.componentContainer.querySelectorAll('input.selcheck');
+            for (var i = 0; i < selchecks.length; i++) {
+                selchecks[i].type = "hidden";
+                if(selchecks[i].checked == true){
+                    selchecks[i].value = "on";
+                }
+            }
+            var selectables = this.componentContainer.querySelectorAll('.selectable');
+            for (var i = 0; i < selectables.length; i++) {
+                if (selectables[i].id) {
+                    selectables[i].addEventListener('click', function() {
+                        selectWord(event);
+                    });
+                }
+            }
+        }
+
+        if (!this.question) {
+            console.warn('Aborting because of no question received.');
+            return that.CoreQuestionHelperProvider.showComponentError(that.onAbort);
+        }
+
+        const div = document.createElement('div');
+        div.innerHTML = this.question.html;
+
+         // Treat the correct/incorrect icons.
+        this.CoreQuestionHelperProvider.treatCorrectnessIcons(div);
+
+        if (div.querySelector('.readonly') !== null) {
+            this.question.readonly = true;
+        }
+        if (div.querySelector('.feedback') !== null) {
+            this.question.feedback = div.querySelector('.feedback');
+            this.question.feedbackHTML = true;
+        }
+
+        this.question.text = this.CoreDomUtilsProvider.getContentsOfElement(div, '.qtext');
+
+        if (typeof this.question.text == 'undefined') {
+            this.logger.warn('Aborting because of an error parsing question.', this.question.name);
+            return this.CoreQuestionHelperProvider.showComponentError(this.onAbort);
+        }
+
+        return true;
+    }
+};
+result;
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_wordselect';
-$plugin->version  = 2018120300;
+$plugin->version  = 2018121500;
 $plugin->requires = 2015051109;  // Moodle 2.9.9.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = '2.32';
+$plugin->release  = '2.3';
