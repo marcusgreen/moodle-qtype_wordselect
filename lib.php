384bd16c4a114aba74eec6e1a139504ecfa24f51
marcusgreen <marcusavgreen@gmail.com>
2019-04-11 11:22:46 +0100

    example of fragment from documentation

--- lib.php
+++ lib.php
@@ -46,3 +46,30 @@ function qtype_wordselect_pluginfile($course, $cm, $context, $filearea, $args, $
     require_once($CFG->libdir . '/questionlib.php');
     question_pluginfile($course, $context, 'qtype_wordselect', $filearea, $args, $forcedownload, $options);
 }
+function qtype_wordselect_output_fragment_questionedit($args) {
+    global $CFG;
+
+    $context = $args['context'];
+ /*
+    if ($context->contextlevel != CONTEXT_MODULE) {
+        return null;
+    }
+    require_once($CFG->dirroot . '/mod/assign/locallib.php');
+    $assign = new assign($context, null, null);
+ 
+    $userid = clean_param($args['userid'], PARAM_INT);
+    $attemptnumber = clean_param($args['attemptnumber'], PARAM_INT);
+    $formdata = array();
+    if (!empty($args['jsonformdata'])) {
+        $serialiseddata = json_decode($args['jsonformdata']);
+        parse_str($serialiseddata, $formdata);
+    }
+    $viewargs = array(
+        'userid' => $userid,
+        'attemptnumber' => $attemptnumber,
+        'formdata' => $formdata
+    );
+ 
+    return $assign->view('gradingpanel', $viewargs);
+    */
+}
