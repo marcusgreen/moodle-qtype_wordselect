8f51eb95a696e018de0c6a1b84516a7550004f6f
Marcus Green <marcusavgreen@gmail.com>
2020-11-14 09:47:51 +0000

    fixes to satisfy travis

--- renderer.php
+++ renderer.php
@@ -41,7 +41,6 @@ class qtype_wordselect_renderer extends qtype_with_combined_feedback_renderer {
      * @return string HTML fragment.
      */
     public function formulation_and_controls(question_attempt $qa, question_display_options $options) {
-        global $PAGE;
 
         $output = '';
 
@@ -163,7 +162,7 @@ class qtype_wordselect_renderer extends qtype_with_combined_feedback_renderer {
         if ($qa->get_state() == question_state::$invalid) {
             $output .= html_writer::div($question->get_validation_error($response), 'validationerror');
         }
-        $PAGE->requires->js_call_amd('qtype_wordselect/selection', 'init',
+        $this->page->requires->js_call_amd('qtype_wordselect/selection', 'init',
         [$qa->get_outer_question_div_unique_id()]);
 
         return $output;
