33b382d0a61c6317f5755a3e2f86edb05d100769
Marcus Green <marcusgreen@marcuss-mbp.home>
2021-05-30 06:58:29 +0100

    version bump

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_wordselect';
-$plugin->version  = 2021052800;
+$plugin->version  = 2021052900;
 $plugin->requires = 2019052000;  // Moodle 3.7.0.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release  = '2.45';
