5157841ebd1f123df4e175cffbd03474a4699cd4
Marcus Green <marcusavgreen@gmail.com>
2024-04-29 09:22:13 +0100

    Update version.php version bump

--- version.php
+++ version.php
@@ -18,14 +18,14 @@
  * wordselect question type version information.
  *
  * @package    qtype_wordselect
- * @copyright  Marcus Green 2023
+ * @copyright  Marcus Green 2024
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_wordselect';
-$plugin->version  = 2023102200;
+$plugin->version  = 20240429;
 $plugin->requires = 2022040100;  // Moodle 4.0.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = '2.50';
+$plugin->release  = '2.52';
 
